14e978a829b7eaadaf368cfe7d3fb7002fccad0b
Nela <yuna@garasilabs.com>
2021-06-19 01:19:09 +0700

    add active user widget

--- app/components/Dashboard/Deposits.js
+++ app/components/Dashboard/Deposits.js
@@ -2,26 +2,98 @@
 import React from 'react';
 // import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import {
+  BarChart,
+  Bar,
+  // eslint-disable-next-line import/no-unresolved
+} from 'recharts';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+function createData(id, date, amount) {
+  return { id, date, amount };
+}
+
+const rows = [
+  createData(0, '/#/', 126),
+  createData(1, '/#/kabar-covid', 941),
+  createData(2, '/#/info-vaksin', 712),
+];
 
 export default function Deposits() {
   return (
     <React.Fragment>
-      <Typography component="p" variant="h5" align="center">
-        Global Case
-      </Typography>
-      <Title>Confirmed</Title>
-      <Typography component="p" variant="h7">
-        177500592
-      </Typography>
-      <Title>Recovered</Title>
-      <Typography component="p" variant="h7">
-        115584768
-      </Typography>
-      <Title>Deaths</Title>
-      <Typography component="p" variant="h7">
-        3843729
-      </Typography>
+      <Typography component="p">Active User Right Now</Typography>
+      <Title>479</Title>
+      <Typography component="p">Pages view per minute</Typography>
+      <BarChart width={150} height={40} data={data}>
+        <Bar dataKey="uv" fill="#8884d8" />
+      </BarChart>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Top Active Pages</TableCell>
+            <TableCell align="right">Active Users</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.date}</TableCell>
+              <TableCell align="right">{row.amount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </React.Fragment>
   );
 }
